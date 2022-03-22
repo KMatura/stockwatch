3e7eee5b6dd59a9486546cd5659f0df5824e5372
DaniJa03 <jaber.d03@htlwienwest.at>
2022-03-22 11:20:41 +0100

    nodemon + vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,1 +1,16 @@
-//nicht fertig
+import { fileURLToPath, URL } from 'url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  server: {
+    port: 80,
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,1 +1,4 @@
 //routes
+export default{
+
+}
