cf34f91936239d6990349b60799094822d5d5a84
kmatura <matura.k04@htlwienwest.at>
2022-03-29 12:42:06 +0200

    projekt neu erstellt, vite gefixed, navbar fixed, routerview fehlt

--- server/routes/stock.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getStockRealtime } from '../controllers/getStockdata.js';
-
-const router = Router();
-
-router.get('/getStockRealtime', getStockRealtime);
-
-export { router }
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NavBar></NavBar>
+    <!-- <h6 class="text-center fs-1">Chart...</h6>
+    <TrendingStocks></TrendingStocks>
+    <LineChart></LineChart> -->
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+}
+};
+</script>
+
+<style></style>
