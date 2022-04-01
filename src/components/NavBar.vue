d0b1077c8e81432a9987b60472531a7ba81cefc8
DaniJa03 <jaber.d03@htlwienwest.at>
2022-04-01 12:54:03 +0200

    ChartCards hinzugefügt + responsive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -18,9 +18,11 @@ import { RouterLink, createWebHistory, RouterView } from 'vue-router';
             <RouterLink to="/faq">FAQ</RouterLink>
           </a>
         </div>
-        <div class="flex space-x-3 items-center">
-          <img src="../assets/stockwatch_logo.png" class="w-14" alt="circle" />
-          <h1 class="font-bold text-2xl leading-6 text-gray-800">StockWatch</h1>
+        <div class="flex space-x-3 items-center" href="#">
+            <img src="../assets/stockwatch_logo.png" class="w-14" alt="StockWatchLogo" />
+            <h1 class="font-bold text-2xl leading-6 text-gray-800" href="#">
+              <RouterLink to="/">StockWatch</RouterLink>
+            </h1>
         </div>
         <div class="hidden sm:flex flex-row space-x-4">
           <button
@@ -31,7 +33,7 @@ import { RouterLink, createWebHistory, RouterView } from 'vue-router';
           <button
             class="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-white bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-600 hover:bg-indigo-600 duration-150 justify-center items-center"
           >
-          <RouterLink to="/login">Login</RouterLink>
+            <RouterLink to="/login">Login</RouterLink>
           </button>
         </div>
 
@@ -53,33 +55,26 @@ import { RouterLink, createWebHistory, RouterView } from 'vue-router';
       <!-- Mobile and small-screen devices (toggle Menu) -->
       <div id="MobileNavigation" class="hidden sm:hidden mt-4 mx-auto">
         <div class="flex flex-row items-center justify-center space-x-6">
-          <img
-            src="https://tuk-cdn.s3.amazonaws.com/can-uploader/header-3-svg1.svg"
-            alt="twitter"
-          />
-          <img
-            src="https://tuk-cdn.s3.amazonaws.com/can-uploader/header-3-svg2.svg"
-            alt="facebook"
-          />
-          <img
-            src="https://tuk-cdn.s3.amazonaws.com/can-uploader/header-3-svg3.svg"
-            alt="linkdin"
-          />
-          <img
-            src="https://tuk-cdn.s3.amazonaws.com/can-uploader/header-3-svg4.svg"
-            alt="instagram"
-          />
+          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+            <RouterLink to="/dashboard">Dashboard</RouterLink>
+          </a>
+          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+            <RouterLink to="/settings">Settings</RouterLink>
+          </a>
+          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+            <RouterLink to="/faq">FAQ</RouterLink>
+          </a>
         </div>
         <div class="flex flex-col gap-4 mt-4 w-80 mx-auto">
           <button
             class="rounded-md flex space-x-2 w-full h-10 font-normal text-sm leading-3 text-indigo-700 bg-indigo-600 bg-opacity-0 hover:opacity-100 duration-100 border border-indigo-700 focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center"
           >
-            Sign Up
+            <RouterLink to="/signup">Sign Up</RouterLink>
           </button>
           <button
             class="rounded-md flex space-x-2 w-full h-10 font-normal text-sm leading-3 text-white bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-600 hover:bg-indigo-600 duration-150 justify-center items-center"
           >
-            Sign In
+            <RouterLink to="/signin">Sign In</RouterLink>
           </button>
         </div>
       </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <NavBar></NavBar>
+    <TrendingStocks></TrendingStocks>
     <!-- <LineChart></LineChart> -->
   </div>
 </template>
 
 <script setup>
 import NavBar from '../components/NavBar.vue';
-// import LineChart from '../components/LineChart.vue'
+import TrendingStocks from '../components/TrendingStocks.vue';
+// import LineChart from '../components/LineChart.vue';
 </script>
