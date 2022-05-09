6d29851649e32bcdfd7ee844f7347f4bfcceb2d6
DaniJa03 <jaber.d03@htlwienwest.at>
2022-05-09 21:09:23 +0200

    search bar add

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="py-4 px-7">
+    <div class="flex justify-center">
+      <div class="flex justify-center pb-2 lg:flex-nowrap">
+        <div class="flex flex-col w-full gap-4 lg:flex-row md:flex-col p-7">
+          <div class="relative lg:max-w-[410px] w-full">
+            <input
+              placeholder="Search"
+              class="p-4 py-3 outline-none focus pr-10 bg-gray-100 border rounded border-gray-100 text-slate-600 lg:max-w-[410px] w-full leading-4"
+            />
+            <svg
+              class="absolute pointer-events-none top-3 right-5"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17Z"
+                stroke="#4B5563"
+                stroke-width="1.66667"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M21 21L15 15"
+                stroke="#4B5563"
+                stroke-width="1.66667"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+          <button
+            class="bg-indigo-700 text-white lg:max-w-[164px] font-medium px-6 py-4 w-full rounded-[4px] leading-[14px] hover:bg-indigo-600"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,12 @@
   <div>
     <NavBar></NavBar>
     <TrendingStocks></TrendingStocks>
+    <SearchBar></SearchBar>
   </div>
 </template>
 
 <script setup>
 import NavBar from '../components/NavBar.vue';
 import TrendingStocks from '../components/TrendingStocks.vue';
+import SearchBar from '../components/SearchBar.vue';
 </script>
